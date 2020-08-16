50ed3dd2ea5e08e384a286c345214303a8933899
Hyun Ahn <codepawn@icloud.com>
2020-08-16 16:43:16 +0900

    first commit plant_go

---
+++ lib/components/recommend_plant_card/recommend_plant_cards.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:plant_go/components/recommend_plant_card/recommend_plant_card.dart';
+
+import 'package:plant_go/components/details/details.dart';
+
+class RecommendPlantCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                RecommendPlantCard(
+                  image: 'assets/images/image_1.png',
+                  title: 'Samantha',
+                  country: "Russia",
+                  price: 440,
+                  press: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Details(),
+                      ),
+                    );
+                  },
+                ),
+                RecommendPlantCard(
+                  image: 'assets/images/image_2.png',
+                  title: 'Samantha',
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
+                RecommendPlantCard(
+                  image: 'assets/images/image_3.png',
+                  title: 'Samantha',
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
+                RecommendPlantCard(
+                  image: 'assets/images/image_1.png',
+                  title: 'Samantha',
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
+                RecommendPlantCard(
+                  image: 'assets/images/image_2.png',
+                  title: 'Samantha',
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
+                RecommendPlantCard(
+                  image: 'assets/images/image_3.png',
+                  title: 'Samantha',
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
